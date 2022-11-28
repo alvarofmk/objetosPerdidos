8c6ae3f5de601c370da2b0bc3e748876ac02e389
alvarofmk <alvarofmk@gmail.com>
2022-11-28 13:29:59 +0100

    Trabajando en firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5LdeXgnRZydcPNg2MZzUTTOsgcR3GSAw",
+    authDomain: "objetos-perdidos-723bd.firebaseapp.com",
+    projectId: "objetos-perdidos-723bd",
+    storageBucket: "objetos-perdidos-723bd.appspot.com",
+    messagingSenderId: "956672083774",
+    appId: "1:956672083774:web:18efe979af29e333d60e6f"
+  }
 };
 
 /*
